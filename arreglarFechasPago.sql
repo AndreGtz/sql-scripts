abceca091ab0a1d4a4776d0ecf85a75923018a54
Andre Gutierrez <agutierrez@tresmarias.com.mx>
2015-09-29 16:41:20 -0500

    Algunos arreglos mas

--- arreglarFechasPago.sql
+++ arreglarFechasPago.sql
@@ -1,5 +1,13 @@
+## Arreglar fechas
+set @noSolicitud = 2045;#2521
+set @cveContratacion = (select cveContratacion from contratacion where noSolicitud = @noSolicitud);
+set @pagoFrom = 1;
+set @pagoTo = 49;
+select @noSolicitud,@cveContratacion,@pagoFrom,@pagoTo;
+
+select * from planpagos where cveContratacion = @cveContratacion;
 select noAportacion,fechaPago,noPago from planpagos where cveContratacion = @cveContratacion;
 #arreglar fechas de pago
-update planpagos set noAportacion = (noAportacion-29) where noPago>=10 and cveContratacion = @cveContratacion;
-update planpagos set fechaPago = (select fecha from quincenas where cveQuincena = planpagos.noAportacion) where cveContratacion = @cveContratacion 
-;
+update planpagos set noAportacion = (noAportacion+2) where noPago>=@pagoFrom and noPago<=@pagoTo and cveContratacion = @cveContratacion;
+update planpagos set fechaPago = (select fecha from quincenas where cveQuincena = planpagos.noAportacion) where cveContratacion = @cveContratacion and noPago>=@pagoFrom and noPago<=@pagoTo;
+
