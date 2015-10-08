96a95cb49cc9be91e71a1772ed837bd2146b7fdf
Andre Gutierrez <agutierrez@tresmarias.com.mx>
2015-10-08 13:01:47 -0500

    cambios de formato

--- arreglarLayouts.sql
+++ arreglarLayouts.sql
@@ -1,10 +1,17 @@
 ##########################
 # Arreglar layouts       #
 ##########################
+
+
+#variables
 set @noSolicitud = 6579;#2521
 set @cveContratacion = (select cveContratacion from contratacion where noSolicitud = @noSolicitud);
+
+
 # cambiar manualmente en planpagos las cveLayout
 select * from planpagos where cveContratacion = @cveContratacion;
+
+
 # actualizar las tablas cobranza,pagoslayout
 update pagoslayout set cveLayout = 
 (select planpagos.cveLayout from planpagos 
@@ -18,6 +25,8 @@ update pagoslayout set cveLayout =
 		and planpagos.statusRegistro = 1
 		and planpagos.noPago = pagoslayout.noPago)  
 where cveContratacion = @cveContratacion;
+
+
 #si hay registros en planpagos que no hay en pagoslayout insertarlos
 insert into pagoslayout (cvePago,cveContratacion,cveConcepto,interes,cveUsrReg,fechaReg,status,cveConvenio,cveContacto,saldo,iva,noAportacion,tipoAportacion,aplicaInteres,statusRegistro,montoProg,devengado,fechaDev,hrDev,fechaPago,capital,cantidadPagada,saldoFinalTotal,tipoTransaccion,noPago,cveLayout,observaciones)
 (select cvePago,cveContratacion,cveConcepto,interes,cveUsrReg,fechaReg,status,cveConvenio,cveContacto,saldo,iva,noAportacion,tipoAportacion,aplicaInteres,statusRegistro,montoProg,devengado,fechaDev,hrDev,fechaPago,capital,cantidadPagada,saldoFinalTotal,tipoTransaccion,noPago,cveLayout,observaciones 
